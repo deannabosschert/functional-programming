5389684d68947ba92b737c5d6fe07d7606a15d33
techkech <deanna.bosschert@upcmail.nl>
2020-10-27 15:15:32 +0100

    feat: renders modules

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,26 @@
 // const dataDingen = require('./src/app.js')
 
-module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy({'src/assets/css': 'assets/css'})
-  eleventyConfig.addPassthroughCopy({'src/assets/img': 'assets/img'})
-  eleventyConfig.addPassthroughCopy({'src/app.js': 'app.js'})
-  eleventyConfig.addPassthroughCopy({'lib': 'lib'})
-  eleventyConfig.addPassthroughCopy({'src/data': 'data'})
+// module.exports = (eleventyConfig) => {
+//   eleventyConfig.addPassthroughCopy({'public/css': 'assets/css'})
+//   eleventyConfig.addPassthroughCopy({'public/img': 'assets/img'})
+//   eleventyConfig.addPassthroughCopy({'lib': 'lib'})
+//
+//   return {
+//     dir: {
+//       input: 'views',
+//       output: 'docs'
+//     },
+//     templateFormats: ['html', 'ejs', 'md', '11ty.js'],
+//     htmlTemplateEngine: 'ejs',
+//     markdownTemplateEngine: 'ejs',
+//     passthroughFileCopy: true
+//   }
+// }
 
-  return {
-    dir: {
-      input: 'src',
-      output: 'docs'
-    },
-    templateFormats: ['html', 'md', '11ty.js'],
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
-    passthroughFileCopy: true
-  }
-}
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('img')
+    return {
+        passthroughFileCopy: true
+    };
+};
