e383a2d551c8e47b72993c338439fe22c5f9d2ba
techkech <deanna.bosschert@upcmail.nl>
2020-10-29 13:39:53 +0100

    fix: file path

--- src/_data/eyeColors.js
+++ src/_data/eyeColors.js
@@ -1,4 +1,4 @@
-const CleanEyecolor = require('../lib/filters/clean-eyecolor.js')
+const CleanEyecolor = require('../lib/clean-eyecolor.js')
 // export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde
 // dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
 module.exports = async () => {
