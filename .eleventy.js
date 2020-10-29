acc346bc4564785626e73841432bd96392b6a043
techkech <deanna.bosschert@upcmail.nl>
2020-10-29 13:31:32 +0100

    refactor: remove unused code

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,3 @@
-// const dataDingen = require('./src/app.js')
-
-// module.exports = (eleventyConfig) => {
-//   eleventyConfig.addPassthroughCopy({'public/css': 'assets/css'})
-//   eleventyConfig.addPassthroughCopy({'public/img': 'assets/img'})
-//   eleventyConfig.addPassthroughCopy({'lib': 'lib'})
-//
-//   return {
-//     dir: {
-//       input: 'views',
-//       output: 'docs'
-//     },
-//     templateFormats: ['html', 'ejs', 'md', '11ty.js'],
-//     htmlTemplateEngine: 'ejs',
-//     markdownTemplateEngine: 'ejs',
-//     passthroughFileCopy: true
-//   }
-// }
-
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('assets/fonts')
   eleventyConfig.addPassthroughCopy('assets/img')
@@ -31,4 +12,4 @@ module.exports = (eleventyConfig) => {
     htmlTemplateEngine: 'njk',
     passthroughFileCopy: true
   }
-};
+}
