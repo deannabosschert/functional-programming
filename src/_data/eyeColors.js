72db129ce0e3e80abbe8823fd55bc9cb0eb45cf1
techkech <deanna.bosschert@upcmail.nl>
2020-10-29 13:16:19 +0100

    refactor: add code comments

--- src/_data/eyeColors.js
+++ src/_data/eyeColors.js
@@ -1,5 +1,6 @@
 const CleanEyecolor = require('../lib/filters/clean-eyecolor.js')
-
+// export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde 
+// dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
 module.exports = async () => {
   return eyeColor = await CleanEyecolor()
 };
