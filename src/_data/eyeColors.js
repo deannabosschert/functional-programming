407301fb532d1c198279c1d1cd18857c2d26a36a
“deannabosschert” <deanna.bosschert@gmail.com>
2020-10-30 12:14:43 +0100

    feat: render colors with background color

--- src/_data/eyeColors.js
+++ src/_data/eyeColors.js
@@ -1,6 +1,9 @@
-const CleanEyecolor = require('../lib/clean-eyecolor.js')
-// export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde
+const CleanEyecolors = require('../lib/clean-eyecolors.js')
+const RenderColors = require('../lib/render-eyecolors.js')
+
+// export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde en render deze dan
 // dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
 module.exports = async () => {
-  return eyeColor = await CleanEyecolor()
+  return eyeColors = await CleanEyecolors()
+  .then(data => RenderColors(data))
 }
