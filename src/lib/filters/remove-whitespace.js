ff08bfff9f5df5e3d99bcac638692523db65441d
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-01 22:25:22 +0100

    refactor: change let to const

--- src/lib/filters/remove-whitespace.js
+++ src/lib/filters/remove-whitespace.js
@@ -1,6 +1,6 @@
 module.exports = function removeWhitespace(data) { // remove excessive tabs at the start or end of a value
   return data.map((person) => {
-    let trimmedEyeColor = person.eyeColor.trim()
+    const trimmedEyeColor = person.eyeColor.trim()
     return {eyeColor: trimmedEyeColor}
   })
 }
