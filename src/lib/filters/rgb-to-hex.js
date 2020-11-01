7c9ed2ef76837280c4c7a515b38ea11135562cb5
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-01 23:13:32 +0100

    refactor: change variable name

--- src/lib/filters/rgb-to-hex.js
+++ src/lib/filters/rgb-to-hex.js
@@ -1,8 +1,8 @@
 const rgbHex = require('rgb-hex')
 
 module.exports = function rgbToHex(data) { // check for rgb answers, then convert them to hex
-  return data.map((person) => {
-    let eyeColor = person.eyeColor
+  return data.map((item) => {
+    let eyeColor = item.eyeColor
     const matches = eyeColor.substring(1, 4).match(/rgb+/g)
     if (matches == 'rgb' || matches == 'RGB') {
       const hexColor = matchRGB(eyeColor)
