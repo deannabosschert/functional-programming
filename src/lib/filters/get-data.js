7b19d2898560f62099cb031c9d961ed5ab790a86
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-01 23:13:19 +0100

    refactor: change variable name 'person' to 'item'

--- src/lib/filters/get-data.js
+++ src/lib/filters/get-data.js
@@ -1,7 +1,7 @@
 module.exports = function getData(datasource, colors) { // get data from questionnaire file
   return new Promise((resolve, reject) => {
-    const colors = datasource.map(person => ({
-      eyeColor: person.oogKleur
+    const colors = datasource.map(item => ({
+      eyeColor: item.oogKleur
     }))
     resolve(colors)
   })
