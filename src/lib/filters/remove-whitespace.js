1ee73fe184a6f8efed2abf27c624bd47b380d730
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-01 23:12:15 +0100

    refactor: change variable name

--- src/lib/filters/remove-whitespace.js
+++ src/lib/filters/remove-whitespace.js
@@ -1,6 +1,6 @@
 module.exports = function removeWhitespace(data) { // remove excessive tabs at the start or end of a value
-  return data.map((person) => {
-    const trimmedEyeColor = person.eyeColor.trim()
-    return {eyeColor: trimmedEyeColor}
+  return data.map((item) => {
+    const trimmed = item.eyeColor.trim()
+    return {eyeColor: trimmed}
   })
 }
