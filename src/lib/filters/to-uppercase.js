53f1516f9a0b8b4949994163a00153145f93cf78
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-02 17:25:54 +0100

    refactor: reduce amount of lines

--- src/lib/filters/to-uppercase.js
+++ src/lib/filters/to-uppercase.js
@@ -1,6 +1,5 @@
 module.exports = function toUpperCase(data, variable) { // convert all values to uppercase
   return data.map((item) => {
-    let uppercase = item[variable].toUpperCase()
-    return {[variable]: uppercase}
+    return {[variable]: item[variable].toUpperCase()}
   })
 }
