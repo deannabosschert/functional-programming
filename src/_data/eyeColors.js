4706056b802c785360788bae3ddbdf6484793552
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-02 17:48:39 +0100

    feat: add favorite colors

--- src/_data/eyeColors.js
+++ src/_data/eyeColors.js
@@ -1,9 +1,9 @@
-const CleanEyecolors = require('../lib/clean-eyecolors.js')
-const RenderColors = require('../lib/render-eyecolors.js')
+const CleanColors = require('../lib/clean-colors.js')
+const RenderColors = require('../lib/render-colors.js')
 
 // export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde en render deze dan
 // dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
 module.exports = async () => {
-  return eyeColors = await CleanEyecolors()
-  .then(data => RenderColors(data))
+  return eyeColors = await CleanColors('eyeColor')
+  .then(data => RenderColors(data, 'eyeColor'))
 }
