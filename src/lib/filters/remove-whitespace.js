70bd71b540df749d6f50ab497fc406e8f093e036
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-02 17:24:36 +0100

    refactor: reduce amount of lines

--- src/lib/filters/remove-whitespace.js
+++ src/lib/filters/remove-whitespace.js
@@ -1,6 +1,5 @@
 module.exports = function removeWhitespace(data, variable) { // remove excessive tabs at the start or end of a value
   return data.map((item) => {
-    const trimmed = item[variable].trim()
-    return {[variable]: trimmed}
+    return {[variable]: item[variable].trim()}
   })
 }
