6bc8a3d32bab809b12e5b4ddf05cef48a355dbba
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-03 18:09:11 +0100

    fix: remove render-import

--- src/_data/bedroomSizes.js
+++ src/_data/bedroomSizes.js
@@ -1,9 +1,6 @@
 const CleanSizes = require('../lib/clean-sizes.js')
-const RenderSizes = require('../lib/render-sizes.js')
 
-// export de async functie, die wacht dan weer to CleanSizes is uitgevoerd, return dan de door die functie gereturnde waarde en render deze dan
-// dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
+// export de async functie, die wacht dan weer to CleanSizes is uitgevoerd, return dan de door die functie gereturnde waarde
 module.exports = async () => {
   return eyesizes = await CleanSizes('bedroomSize')
-  // .then(data => return data)
 }
