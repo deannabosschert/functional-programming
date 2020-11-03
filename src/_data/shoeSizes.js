10ab251f60e8a540ac534583db32e0e2ad6e7089
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-03 18:36:17 +0100

    feat: add shoe sizes aswell

--- src/_data/shoeSizes.js
+++ src/_data/shoeSizes.js
@@ -2,5 +2,5 @@ const CleanSizes = require('../lib/clean-sizes.js')
 
 // export de async functie, die wacht dan weer to CleanSizes is uitgevoerd, return dan de door die functie gereturnde waarde
 module.exports = async () => {
-  return bedroomSizes = await CleanSizes('bedroomSize')
+  return shoeSizes = await CleanSizes('shoeSize')
 }
