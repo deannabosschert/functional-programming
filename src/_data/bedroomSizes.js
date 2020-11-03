8d3c7c3827ee034f3a5af98975d016cb2ca5eb1b
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-03 02:49:00 +0100

    refactor: adjust code comments to new function names

--- src/_data/bedroomSizes.js
+++ src/_data/bedroomSizes.js
@@ -1,9 +1,9 @@
 const CleanSizes = require('../lib/clean-sizes.js')
 const RenderSizes = require('../lib/render-sizes.js')
 
-// export de async functie, die wacht dan weer to CleanEyecolor is uitgevoerd, return dan de door die functie gereturnde waarde en render deze dan
+// export de async functie, die wacht dan weer to CleanSizes is uitgevoerd, return dan de door die functie gereturnde waarde en render deze dan
 // dit is async aangezien er komende week nog meer uit te voeren functies bijkomen die asynchroon uitgevoerd mogen worden
 module.exports = async () => {
   return eyesizes = await CleanSizes('bedroomSize')
-  .then(data => RenderSizes(data, 'bedroomSize'))
+  // .then(data => return data)
 }
