fd238ead00fe88578454c1212832077d3f7741b7
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-04 03:41:11 +0100

    feat: add rdwDatasets function

---
+++ src/lib/clean-rdw.js
@@ -0,0 +1,16 @@
+const questionnaire = require('../_data/datasets/Survey_Information_Design_clean-parsed.js')
+const getData = require('./filters/get-data.js')
+const removeWhitespace = require('./filters/remove-whitespace.js')
+const pointToComma = require('./filters/point-to-comma.js')
+
+// I should refactor everything in a way that I map over the values at first, then filter and reduce instead of repeating to map
+
+module.exports = async (variable) => {
+  try {
+    return cleanColors = await getData(questionnaire, variable) // wait for data
+      .then(data => removeWhitespace(data, variable)) // remove whitespace
+      .then(data => pointToComma(data, variable))
+  } catch (err) {
+    console.error(err)
+  }
+}
