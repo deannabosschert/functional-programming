a836f05736f259debf54fce9ca306433257afb65
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-04 21:59:26 +0100

    feat: remove console.log

--- src/_data/rdwDatasets.js
+++ src/_data/rdwDatasets.js
@@ -2,7 +2,7 @@ const CleanData = require('../lib/clean-rdw.js')
 
 module.exports = async () => {
   const rdwDataset = await CleanData()
-  console.log(rdwDataset)
+  // console.log(rdwDataset)
 
   return rdwDataset
 }
