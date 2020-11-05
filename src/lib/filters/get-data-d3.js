baeeacb933c9b418c552fad332e7b37d81957369
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-05 15:44:26 +0100

    feat:inbetween-commit

--- src/lib/filters/get-data-d3.js
+++ src/lib/filters/get-data-d3.js
@@ -1,9 +1,13 @@
-// const d3 = require('d3')
+const d3 = require('d3')
 const fetch = require("node-fetch");
 
 module.exports = function getData(url) {
-  return fetch(url).then(response => response.json()).then((data) => {
+  // return fetch(url).then(response => response.json()).then((data) => {
+  //   console.log("Retrieved " + data.length + " records from the dataset!")
+  //   return data
+  // })
+  return d3.json(url).then(function(data) {
     console.log("Retrieved " + data.length + " records from the dataset!")
     return data
-  });
+  })
 }
