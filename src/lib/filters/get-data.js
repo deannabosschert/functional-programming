e89500db7823f473da80b6cd6b09fe50cd552ce8
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-09 16:14:29 +0100

    refactor: change .map to .filter

--- src/lib/filters/get-data.js
+++ src/lib/filters/get-data.js
@@ -1,6 +1,6 @@
 module.exports = function getData(datasource, variable) { // get data from questionnaire file
   return new Promise((resolve, reject) => {
-    const colors = datasource.map(item => ({
+    const colors = datasource.filter(item => ({
       [variable]: item[variable]
     }))
     resolve(colors)
