49d68a86342a1865693804202f5ae12b408ea9e5
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-09 12:27:26 +0100

    feat: remove url

--- src/lib/clean-rdw.js
+++ src/lib/clean-rdw.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const getData = require('./filters/get-rdw-data.js')
-// const query = "adw6-9hsg" // use this when switching to fetching data from API
-// const limit = 2
-// const appToken = process.env.APP_TOKEN
-// const url = `https://opendata.rdw.nl/resource/${query}.json?$limit=${limit}&$$app_token=${appToken}`
-const url = require('../_data/datasets/testlocaties_24_augustus_adressen.json')
+const query = "adw6-9hsg" // use this when switching to fetching data from API
+const limit = 2
+const appToken = process.env.APP_TOKEN
+const url = `https://opendata.rdw.nl/resource/${query}.json?$limit=${limit}&$$app_token=${appToken}`
+// const url = require('../_data/datasets/testlocaties_24_augustus_adressen.json')
 
 
 module.exports = async () => {
