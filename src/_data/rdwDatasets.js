c4dda7ecd63dd91e02ce00670bbc30aee01f2336
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-09 12:29:39 +0100

    feat: remove rdwdata

--- src/_data/rdwDatasets.js
+++ src/_data/rdwDatasets.js
@@ -1,8 +1,8 @@
-const CleanData = require('../lib/clean-rdw.js')
-
-module.exports = async () => {
-  const rdwDataset = await CleanData()
-  // console.log(rdwDataset)
-
-  return rdwDataset
-}
+// const CleanData = require('../lib/clean-rdw.js')
+//
+// module.exports = async () => {
+//   const rdwDataset = await CleanData()
+//   // console.log(rdwDataset)
+//
+//   return rdwDataset
+// }
