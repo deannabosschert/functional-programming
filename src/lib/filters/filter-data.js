2be0bb4c356f43aaeed1075ba2152bbc2eaa76f8
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-12 00:51:42 +0100

    refactor: return immediately

--- src/lib/filters/filter-data.js
+++ src/lib/filters/filter-data.js
@@ -1,6 +1,5 @@
 module.exports = function filterData(data, variable) { // filter data from questionnaire file
-  const colors = data.filter(item => ({
+  return data.filter(item => ({
     [variable]: item[variable]
   }))
-  return colors
 }
