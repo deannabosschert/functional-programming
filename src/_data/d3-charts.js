4bea00c12fb6d725823a73929bb84a0868bda20e
“deannabosschert” <deanna.bosschert@gmail.com>
2020-11-13 16:14:58 +0100

    feat: this is the final countdown

---
+++ src/_data/d3-charts.js
@@ -0,0 +1,19 @@
+// // const CleanSizes = require('clean-sizes.js')
+//   // const shoeSizes = require('../_data/shoeSizes.js')
+//   // import testDataingen from '../_data/shoeSizes.js';
+//   import d3 from {d3}
+//     var testData = [4, 8, 15, 16, 23, 42];
+//     var x = d3.scale.linear()
+//       .domain([0, d3.max(testData)])
+//       .range([0, 420]);
+//
+//     d3.select(".shoeChart")
+//       .selectAll("div")
+//       .data(testData)
+//       .enter().append("div")
+//       .style("width", function(d) {
+//         return x(d) + "px";
+//       })
+//       .text(function(d) {
+//         return d;
+//       });
